a4749f82b9e8d6e9df884186f27c70f5538bbf54
👋 Hi, I'm Dera Opara <victorchidex1@gmail.com>
2026-01-07 16:55:05 +0000

    pipeline working with docker compose

--- README.md
+++ README.md
@@ -1,2 +1,140 @@
 # data-engineering-bootcamp
 data-engineer
+
+
+uv run python ingest_data.py \
+  --pg-user=root \
+  --pg-pass=root \
+  --pg-host=localhost \
+  --pg-port=5432 \
+  --pg-db=ny_taxi \
+  --target-table=yellow_taxi_trips_2021_1 \
+  --year=2021 \
+  --month=1 \
+  --chunksize=100000
+
+
+
+  docker run -it --rm\
+  --network=pg-network \
+  taxi_ingest:v001 \
+    --pg-user=root \
+    --pg-pass=root \
+    --pg-host=pgdatabase \
+    --pg-port=5432 \
+    --pg-db=ny_taxi \
+    --target-table=yellow_taxi_trips_2021_1 \
+    --year=2021 \
+    --month=1 \
+    --chunksize=100000
+
+
+  docker run -it --rm\
+  taxi_ingest:v001 \
+    --pg-user=root \
+    --pg-pass=root \
+    --pg-host=pgdatabase \
+    --pg-port=5432 \
+    --pg-db=ny_taxi \
+    --target-table=yellow_taxi_trips_2021_1 \
+    --year=2021 \
+    --month=1 \
+    --chunksize=100000
+
+
+  docker run -it --rm\
+  --network=pipeline_default \
+  taxi_ingest:v001\
+  --pg-user=root \
+  --pg-pass=root \
+  --pg-host=pgdatabase \
+  --pg-port=5432 \
+  --pg-db=ny_taxi \
+  --target-table=yellow_taxi_trips_2021_1 \
+  --year=2021 \
+  --month=1 \
+  --chunksize=100000
+
+
+
+
+  docker run -it --rm \
+  --pg-user=root \
+  --pg-pass=root \
+  --pg-host=localhost \
+  --pg-port=5432 \
+  --pg-db=ny_taxi \
+  --target-table=yellow_taxi_trips \
+  --year=2021 \
+  --month=1 \
+  --chunksize=100000
+
+
+docker run -it \
+  -e POSTGRES_USER="root" \
+  -e POSTGRES_PASSWORD="root" \
+  -e POSTGRES_DB="ny_taxi" \
+  -v ny_taxi_postgres_data:/var/lib/postgresql \
+  -p 5432:5432 \
+  --network=pg-network \
+  --name pgdatabase \
+  postgres:18
+
+
+
+
+
+✅ THE REAL FIX (works 100%)
+
+You must disable pgAdmin’s secure cookie & enhanced protection when running behind a proxy.
+
+🔧 Stop and remove pgAdmin
+
+'''bash
+docker stop pgadmin
+docker rm pgadmin
+
+'''
+
+🔧 Run pgAdmin with REQUIRED env fixes
+
+'''bash
+docker run -it \
+  --name pgadmin \
+  --network pg-network \
+  -p 8085:80 \
+  -e PGADMIN_DEFAULT_EMAIL=admin@admin.com \
+  -e PGADMIN_DEFAULT_PASSWORD=root \
+  -e PGADMIN_CONFIG_SERVER_MODE=False \
+  -e PGADMIN_CONFIG_MASTER_PASSWORD_REQUIRED=False \
+  -e PGADMIN_CONFIG_ENHANCED_COOKIE_PROTECTION=False \
+  -e PGADMIN_CONFIG_SESSION_COOKIE_SECURE=False \
+  -e PGADMIN_CONFIG_WTF_CSRF_ENABLED=False \
+  dpage/pgadmin4
+
+
+'''
+
+docker run -it \
+  --name pgadmin \
+  --network pg-network \
+  -p 8085:80 \
+  -e PGADMIN_DEFAULT_EMAIL=admin@admin.com \
+  -e PGADMIN_DEFAULT_PASSWORD=root \
+  -e PGADMIN_CONFIG_SERVER_MODE=False \
+  -e PGADMIN_CONFIG_MASTER_PASSWORD_REQUIRED=False \
+  -e PGADMIN_CONFIG_ENHANCED_COOKIE_PROTECTION=False \
+  -e PGADMIN_CONFIG_SESSION_COOKIE_SECURE=False \
+  -e PGADMIN_CONFIG_WTF_CSRF_ENABLED=False \
+  dpage/pgadmin4
+
+
+docker run -it \
+  -e POSTGRES_USER="root" \
+  -e POSTGRES_PASSWORD="root" \
+  -e POSTGRES_DB="ny_taxi" \
+  -v ny_taxi_postgres_data:/var/lib/postgresql \
+  -p 5432:5432 \
+  --network=pg-network \
+  --name pgdatabase \
+  postgres:18
--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,20 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import click
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
 
-pg_user = 'root'
-pg_pass = 'root'
-pg_host = 'localhost'
-pg_port = 5432
-pg_db = 'ny_taxi'
 
-year = 2021
-month = 1
-
-
+# Column dtypes
 dtype = {
     "VendorID": "Int64",
     "passenger_count": "Int64",
@@ -39,36 +32,38 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-url = f'{prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
 
-df = pd.read_csv(
-    url,
-    nrows=100,
-    dtype=dtype,
-    parse_dates=parse_dates
-)
-
-
-def run():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = 5432
-    pg_db = 'ny_taxi'
-
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
-
-    chunksize = 100000
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+def main(
+    pg_user,
+    pg_pass,
+    pg_host,
+    pg_port,
+    pg_db,
+    year,
+    month,
+    target_table,
+    chunksize
+):
+    """
+    Ingest NYC Yellow Taxi data into PostgreSQL using chunked loading.
+    """
 
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
     url = f'{prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
 
-    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
-
+    engine = create_engine(
+        f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}'
+    )
 
     df_iter = pd.read_csv(
         url,
@@ -78,22 +73,26 @@ def run():
         chunksize=chunksize,
     )
 
-    first = True
-    for df_chunk in tqdm(df_iter):
-        if first:
-            df_chunk.head(n=0).to_sql(
-                name= target_table, 
+    first_chunk = True
+
+    for df_chunk in tqdm(df_iter, desc="Ingesting data"):
+        if first_chunk:
+            # Create table schema
+            df_chunk.head(0).to_sql(
+                name=target_table,
                 con=engine,
-                if_exists='replace'
+                if_exists='replace',
+                index=False
             )
-            first = False
+            first_chunk = False
 
         df_chunk.to_sql(
-            name=target_table, 
-            con=engine, 
-            if_exists='append'
+            name=target_table,
+            con=engine,
+            if_exists='append',
+            index=False
         )
-        
+
 
 if __name__ == '__main__':
-    run()
+    main()
